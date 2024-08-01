5434d18baaee81905c23d19ab3fb0c7f9bd917a3
conventionbroken <2668179+conventionbroken@users.noreply.github.com>
2024-08-01 21:03:02 +0200

    fixed: forgot password

--- src/screens/auth/forgot.js
+++ src/screens/auth/forgot.js
@@ -1,16 +1,16 @@
 import React from 'react';
-import { connect } from 'react-redux';
-import { StyleSheet, ScrollView, KeyboardAvoidingView } from 'react-native';
-import { Header, Text, ThemedView } from 'src/components';
+import {connect} from 'react-redux';
+import {StyleSheet, ScrollView, KeyboardAvoidingView} from 'react-native';
+import {Header, Text, ThemedView} from 'src/components';
 import Container from 'src/containers/Container';
 import Input from 'src/containers/input/Input';
 import Button from 'src/containers/Button';
-import { IconHeader, TextHeader } from 'src/containers/HeaderComponent';
-import { authStack } from 'src/config/navigator';
-import { forgotPassword } from 'src/modules/auth/actions';
-import { authSelector } from 'src/modules/auth/selectors';
-import { margin } from 'src/components/config/spacing';
-import { lineHeights } from 'src/components/config/fonts';
+import {IconHeader, TextHeader} from 'src/containers/HeaderComponent';
+import {authStack} from 'src/config/navigator';
+import {forgotPassword} from 'src/modules/auth/actions';
+import {authSelector} from 'src/modules/auth/selectors';
+import {margin} from 'src/components/config/spacing';
+import {lineHeights} from 'src/components/config/fonts';
 import * as colors from 'src/components/config/colors';
 
 class ForgotScreen extends React.Component {
@@ -23,53 +23,63 @@ class ForgotScreen extends React.Component {
     this.state = {
       phone_number: '',
       isSubmit: null,
-      error: ''
+      error: '',
     };
   }
 
   componentDidUpdate(prevProps, prevState) {
-    if (prevProps.getOTPSuccess !== this.props.auth.getOTPSuccess &&
-      prevState.isSubmit === this.state.isSubmit && this.state.isSubmit) {
+    if (
+      prevProps.getOTPSuccess !== this.props.auth.getOTPSuccess &&
+      prevState.isSubmit === this.state.isSubmit &&
+      this.state.isSubmit
+    ) {
       this.props.navigation.navigate(authStack.verify_phone, {
         isForgot: true,
-        phoneNumber: this.state.phone_number
-      })
+        phoneNumber: this.state.phone_number,
+      });
     }
   }
 
   handleSubmit = (t) => {
-    const { phone_number } = this.state;
+    const {phone_number} = this.state;
     if (phone_number != '') {
-      this.setState({
-        ...this.state,
-        error: '',
-        isSubmit: true
-      }, () => {
-        this.props.dispatch(forgotPassword(phone_number));
-      })
+      this.setState(
+        {
+          ...this.state,
+          error: '',
+          isSubmit: true,
+        },
+        () => {
+          this.props.dispatch(forgotPassword(phone_number));
+        },
+      );
     } else {
       this.setState({
         ...this.state,
-        error: t('auth:text_error_empty_phone_number')
-      })
+        error: t('auth:text_error_empty_phone_number'),
+      });
     }
   };
 
   render() {
     const {
-      auth: { pending, pendingForgotPassword, forgotPasswordError },
-      screenProps: { t, theme },
+      auth: {pending},
+      screenProps: {t},
     } = this.props;
 
-    const { phone_number, error } = this.state;
-    const { message, errors } = forgotPasswordError;
+    const {phone_number, error} = this.state;
 
     return (
       <ThemedView isFullView>
         <Header
           backgroundColor={colors.black}
           leftComponent={<IconHeader color={colors.unSelIcon} />}
-          centerComponent={<TextHeader titleStyle={{ color: colors.white }} title={t('auth:text_resetting_password')} />}
+          centerComponent={
+            <TextHeader
+              titleStyle={{color: colors.white}}
+              title={t('auth:text_resetting_password')}
+            />
+          }
         />
         <ScrollView>
           <KeyboardAvoidingView>
@@ -80,12 +90,12 @@ class ForgotScreen extends React.Component {
               <Input
                 placeholder={t('inputs:text_example_username')}
                 value={phone_number}
-                onChangeText={value => this.setState({ phone_number: value })}
+                onChangeText={(value) => this.setState({phone_number: value})}
                 error={error && error}
               />
               <Button
-                buttonStyle={{ backgroundColor: colors.selIcon }}
-                titleStyle={{ color: colors.black }}
+                buttonStyle={{backgroundColor: colors.selIcon}}
+                titleStyle={{color: colors.black}}
                 title={t('auth:text_reset')}
                 containerStyle={styles.margin}
                 loading={pending}
@@ -109,7 +119,7 @@ const styles = StyleSheet.create({
   },
 });
 
-const mapStateToProps = state => {
+const mapStateToProps = (state) => {
   return {
     auth: authSelector(state),
   };
